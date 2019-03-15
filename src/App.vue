b7ba6a419697ea51bd1f1e057361bc2978d1d35c
Julien Paroche <julien.paroche@algolia.com>
2019-03-15 02:24:17 +0100

    First app version

---
+++ src/App.vue
@@ -0,0 +1,107 @@
+<template>
+  <div id="app">
+    <div class="left">
+      <span class="logo">
+        <img alt="Algolia logo" src="./assets/Logo-algolia-nebula-blue@2x.png">
+      </span>
+      <Form v-on:compute="getIndexSize({ ...$event })" />
+    </div>
+    <div class="right">
+      <pre>{{ output }}</pre>
+    </div>
+  </div>
+</template>
+
+<script>
+import Form from './components/Form.vue'
+
+const baseUrl = 'https://secret-fjord-79801.herokuapp.com'
+const computeUrl = baseUrl + '/compute'
+const getUrl = baseUrl + '/get-index-size'
+
+export default {
+  name: 'app',
+  data: function () {
+    return {
+      output: '⏳ Waiting for compute request',
+      requested: false,
+    }
+  },
+  methods: {
+    getIndexSize(formData) {
+      if (!this.requested) {
+        this.output = '🚀 Requesting index size'
+        this.requested = true
+      }
+      fetch(getUrl, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      })
+        .then(res => res.json())
+        .then(res => {
+          if (res.message && res.message === 'No size computation available for this index') {
+            this.computeIndexSize(formData)
+            setTimeout(() => {
+              this.getIndexSize(formData)
+            }, 10000)
+          }
+          else if (res.message && res.message === 'Size computing is on-going. Please try again later') {
+            this.output = '🔄 Computing the index size - be patient'
+            setTimeout(() => {
+              this.getIndexSize(formData)
+            }, 5000)
+          }
+          else {
+            this.output = JSON.stringify(res, null, 2)
+          }
+        })
+        .catch(e => window.console.warn(e))
+    },
+    async computeIndexSize(formData) {
+      fetch(computeUrl, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      })
+        .then(res => res.json())
+        .then(res => {
+          if (res.message && res.message.indexOf('Computing the index size') !== -1) {
+            this.output = '🔄 Computing the index size - be patient'
+          }
+        })
+        .catch(e => window.console.warn(e))
+    }
+  },
+  components: {
+    Form
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin: 20px;
+  display: flex;
+}
+.logo img {
+  width: 200px;
+}
+.left {
+  padding-right: 50px;
+}
+.right {
+  padding-left: 50px;
+  border-left: 1px solid #2c3e50;
+}
+</style>
---
+++ src/components/Form.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="indexsize">
+    <h1>Index Size</h1>
+    <form action="" method="post" v-on:submit.prevent>
+      <p>
+        <span>Cluster: </span>
+        <input type="text" v-model="cluster" placeholder="d38-use, d119-eu..." autocomplete="off" required>
+      </p>
+      <p>
+        <span>App ID: </span>
+        <input type="text" v-model="appId" placeholder="Like RSBCBF0EG8" autocomplete="off" required>
+      </p>
+      <p>
+        <span>Index: </span>
+        <input type="text" v-model="index" placeholder="great-index-name" autocomplete="off" required>
+      </p>
+      <p>
+        <input type="submit" value="Get" v-on:click="$emit('compute', $data)">
+      </p>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Form',
+    data: function () {
+      return {
+        cluster: 'd119-eu',
+        appId: 'RSBCBF0EG8',
+        index: 'versace_full',
+      }
+    },
+    methods: {
+      getState() {
+        return {
+          cluster: this.cluster,
+          appId: this.appId,
+          index: this.index,
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
