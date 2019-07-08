ad957c7542287d7b6a0ab87dbe2a28d3dfbca2ed
Maxime <maxiloc@gmail.com>
2019-07-08 13:48:03 -0700

    update path

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
-app.use('/metaparams', serveStatic(__dirname + "/metaparams/dist"));
+app.use('/metaparams', serveStatic(__dirname + "/packages/metaparams/dist"));
 var port = process.env.PORT || 80;
 app.listen(port);
 console.log('server started '+ port);
