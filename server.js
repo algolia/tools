cede7560b5e1b595f3b8981304200a2e69506b83
Maxime <maxiloc@gmail.com>
2019-07-08 14:21:15 -0700

    force deploy

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 // server.js
 var express = require('express');
-var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
 app.use('/metaparams', serveStatic(__dirname + "/packages/metaparams/dist"));
