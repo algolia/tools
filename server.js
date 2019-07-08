0e930591e460f68306327042879bef5b81a96be6
Maxime <maxiloc@gmail.com>
2019-07-08 14:04:54 -0700

    deploy index manager

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
 app.use('/metaparams', serveStatic(__dirname + "/packages/metaparams/dist"));
+app.use('/index-manager', serveStatic(__dirname + "/packages/index-manager/dist"));
 var port = process.env.PORT || 80;
 app.listen(port);
 console.log('server started '+ port);
