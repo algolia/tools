008f9a347c57c4d58956afd44008c30f9fa9399d
Maxime <maxiloc@gmail.com>
2019-07-19 19:08:58 +0200

    start api logs app

---
+++ packages/api-logs/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="app" class="bg-moon-grey text-base font-sans-alt">
+        <algolia-proxy :enabled="isProduction || true">
+            <api-logs slot="loggedIn" />
+            <div slot="loggedOut" class="h-screen flex justify-center items-center">
+                <div class="flex flex-col justify-center items-center">
+                    <loader-icon class="w-48 h-48 infinte-rotate" />
+                    <div class="mt-20">
+                        Connecting to your Algolia account
+                    </div>
+                </div>
+            </div>
+            <div slot="unauthorized">
+                This an internal algolia app.
+            </div>
+        </algolia-proxy>
+    </div>
+
+</template>
+
+<script>
+    import LoaderIcon from "common/icons/loader.svg";
+    import AlgoliaProxy from "common/components/AlgoliaProxy";
+    import ApiLogs from "@/api-logs/ApiLogs";
+
+    const isProduction = process.env.NODE_ENV === 'production';
+
+    export default {
+        name: 'App',
+        components: {ApiLogs, AlgoliaProxy, LoaderIcon},
+        data: function () {
+            return {isProduction};
+        }
+    }
+</script>
+
+<style lang="scss">
+    @import url("https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700|Hind:400,600");
+    @import "./src/assets/css/main";
+</style>
---
+++ packages/api-logs/src/api-logs/ApiLogs.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        LOGS
+        <pre>
+            {{JSON.stringify(logs, null, 2)}}
+        </pre>
+    </div>
+</template>
+
+<script>
+    import algoliasearch from 'algoliasearch';
+
+    export default {
+        name: 'ApiLogs',
+        data: function () {
+            return {
+                logs: [],
+                client: algoliasearch('AJ0P3S7DWQ', 'ce1181300d403d21311d5bca9ef1e6fb')
+            };
+        },
+        created: async function () {
+            const res = await this.client.getLogs();
+            this.logs = res.logs;
+        }
+    }
+</script>
