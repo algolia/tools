31a644f9eac0bc94017d62e45bbbbda2856411f2
Maxime <maxiloc@gmail.com>
2019-07-19 19:10:11 +0200

    better setup

--- packages/index-manager/vue.config.js
+++ packages/index-manager/vue.config.js
@@ -1,14 +1,23 @@
 module.exports = {
-  publicPath: 'index-manager',
-  chainWebpack: config => {
-    const svgRule = config.module.rule("svg");
-    svgRule.uses.clear();
-    svgRule.use("vue-svg-loader").loader("vue-svg-loader");
+    publicPath: 'index-manager',
+    chainWebpack: config => {
+        const svgRule = config.module.rule("svg");
+        svgRule.uses.clear();
+        svgRule.use("vue-svg-loader").loader("vue-svg-loader");
 
-    config.module
-      .rule("yml")
-      .test(/\.ya?ml$/)
-      .use("js-yaml-loader")
-      .loader("js-yaml-loader");
-  },
+        config.module
+            .rule('eslint')
+            .use('eslint-loader')
+            .tap(options => {
+                options = options || {},
+                    options.configFile = path.resolve(__dirname, "../../.eslintrc");
+                return options;
+            });
+
+        config.module
+            .rule("yml")
+            .test(/\.ya?ml$/)
+            .use("js-yaml-loader")
+            .loader("js-yaml-loader");
+    },
 };
--- packages/metaparams/vue.config.js
+++ packages/metaparams/vue.config.js
@@ -1,25 +1,25 @@
 const path = require('path');
 
 module.exports = {
-  publicPath: 'metaparams',
-  chainWebpack: config => {
-    const svgRule = config.module.rule("svg");
-    svgRule.uses.clear();
-    svgRule.use("vue-svg-loader").loader("vue-svg-loader");
+    publicPath: 'metaparams',
+    chainWebpack: config => {
+        const svgRule = config.module.rule("svg");
+        svgRule.uses.clear();
+        svgRule.use("vue-svg-loader").loader("vue-svg-loader");
 
-    /*config.module
-          .rule('eslint')
-          .use('eslint-loader')
-          .tap(options => {
-              options = options || {},
-              options.configFile = path.resolve(__dirname, "../../.eslintrc");
-              return options;
-          });*/
+        config.module
+            .rule('eslint')
+            .use('eslint-loader')
+            .tap(options => {
+                options = options || {},
+                    options.configFile = path.resolve(__dirname, "../../.eslintrc");
+                return options;
+            });
 
-    config.module
-      .rule("yml")
-      .test(/\.ya?ml$/)
-      .use("js-yaml-loader")
-      .loader("js-yaml-loader");
-  },
+        config.module
+            .rule("yml")
+            .test(/\.ya?ml$/)
+            .use("js-yaml-loader")
+            .loader("js-yaml-loader");
+    },
 };
