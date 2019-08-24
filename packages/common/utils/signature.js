93a42f819b1f709a72c1a5fbfa353a1c57660bd1
Maxime <maxiloc@gmail.com>
2019-08-24 13:05:50 +0200

    (common) add locking system to avoid fetching several times the same signature

--- packages/common/utils/signature.js
+++ packages/common/utils/signature.js
@@ -1,8 +1,19 @@
 const cache = {};
 
+const lock = {};
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 export default async function (appId) {
+    while (lock[appId]) {
+        await sleep(10);
+    }
+
     if (cache[appId]) return cache[appId];
 
+    lock[appId] = true;
     const res = await fetch(`${process.env.VUE_APP_METAPARAMS_BACKEND_ENDPOINT}/signature/${appId}`, {
         credentials: 'include',
         headers: {
@@ -13,6 +24,6 @@ export default async function (appId) {
     const signature = json.signature ? json.signature : '';
 
     cache[appId] = signature;
-
+    lock[appId] = false;
     return signature;
 }
