04a37094b652ed9664aae293e9a6b155808f98ec
Maxime <maxiloc@gmail.com>
2019-08-24 18:56:29 +0200

    (relevance testing) fix server to handle history api

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // server.js
 const express = require('express');
 const serveStatic = require('serve-static');
-const history = require('connect-history-api-fallback');
+const history = require('./customHistory');
 
 app = express();
 
@@ -12,23 +12,22 @@ app.use((req, res, next) => {
         return next();
 });
 
-function redirectUnmatched(req, res) {
-    res.redirect("https://algolia-experimental.herokuapp.com/apps");
-}
+app.use(history({
+    rewrites: [
+        { from: /^\/relevance-testing\/.*$/, to: '/relevance-testing/index.html' }
+    ]
+}));
+
 
 app.use('/apps', serveStatic(__dirname + "/packages/apps/dist"));
 app.use('/logs', serveStatic(__dirname + "/packages/logs/dist"));
 app.use('/metaparams', serveStatic(__dirname + "/packages/metaparams/dist"));
 app.use('/index-manager', serveStatic(__dirname + "/packages/index-manager/dist"));
 app.use('/perso-formula', serveStatic(__dirname + "/packages/perso-formula/dist"));
-app.use(history({
-    verbose: true,
-    rewrites: [
-        { from: /^\/relevance-testing\/suites.*$/, to: '/relevance-testing/index.html' }
-    ]
-}));
 app.use('/relevance-testing', serveStatic(__dirname + "/packages/relevance-testing/dist"));
-app.use(redirectUnmatched);
+app.use((req, res) => {
+    res.redirect("https://algolia-experimental.herokuapp.com/apps");
+});
 
 var port = process.env.PORT || 80;
 app.listen(port);
