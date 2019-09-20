93a6790dd9f467b5d65157be94fb3e4d151d70e4
Maxime <maxiloc@gmail.com>
2019-09-20 16:48:58 +0200

    (index-differ) Bootstrap application

---
+++ packages/index-differ/src/index-differ/IndexDiffer.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <app-header app-name="Index Differ">
+            <display-config class="mx-16 mt-0 ml-auto"/>
+        </app-header>
+        <app-management />
+    </div>
+</template>
+
+<script>
+    import AppHeader from "common/components/header/AppHeader";
+    import DisplayConfig from "@/index-differ/DisplayConfig";
+    import AppManagement from "common/components/configuration/AppManagement";
+
+    export default {
+        name: 'IndexDiffer',
+        components: {DisplayConfig, AppHeader, AppManagement},
+    }
+</script>
---
+++ packages/index-differ/src/main.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import store from 'common/store/store'
+import App from '@/App.vue'
+import algoliasearch from 'algoliasearch';
+import VueInputAutowidth from 'vue-input-autowidth'
+
+Vue.config.productionTip = false;
+Vue.config.performance = true;
+
+Vue.use(VueInputAutowidth);
+
+Vue.mixin({
+    data() {
+        return {
+            algoliaAppID: process.env.VUE_APP_APP_ID,
+            algoliaApiKey: process.env.VUE_APP_SEARCH_ONLY_API_KEY,
+            algoliasearch: algoliasearch
+        }
+    },
+});
+
+new Vue({
+    render: h => h(App),
+    store: store,
+}).$mount('#app');
