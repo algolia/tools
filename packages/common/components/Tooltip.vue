216753b81bf13a09883fcb5aa97a0e7e249d2817
Maxime <maxiloc@gmail.com>
2019-09-28 17:41:23 +0200

    (common) Fix tooltip css

--- packages/common/components/Tooltip.vue
+++ packages/common/components/Tooltip.vue
@@ -1,9 +1,9 @@
 <template>
     <div
         :class="[
-            !position || position === 'center' ? 'inset-x-auto inset-y-full' : '',
-            position === 'left' ? 'pin-l inset-y-full' : '',
-            position === 'right' ? 'pin-r -mr-48 inset-y-full' : '',
+            !position || position === 'center' ? 'inset-x-auto-y-full' : '',
+            position === 'left' ? 'left-0 inset-y-full' : '',
+            position === 'right' ? 'right-0 -mr-48 inset-y-full' : '',
         ]"
         class="absolute z-10 -mt-4 bg-white px-8 py-4 shadow text-solstice-blue text-base border border-proton-grey rounded whitespace-no-wrap hidden group-hover:block"
     >
