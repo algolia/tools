13dc943edde4c4b6a13192351e1de3f9b2370b14
Maxime <maxiloc@gmail.com>
2019-10-02 22:24:15 +0200

    (common:AppSelector) add highlighting in autocomplete

--- packages/common/components/selectors/AppSelector.vue
+++ packages/common/components/selectors/AppSelector.vue
@@ -7,16 +7,19 @@
         :refine="refine"
     >
         <template slot="icon"><box-icon class="block w-12 h-12 -mt-1 mr-8 fill-current"/></template>
-        <template v-slot:default="{option, inDropDown, isSelected}">
+        <template v-slot:default="{option, inDropDown, isSelected, highlightString}">
             <div>
                 <div>
-                    {{option}}
+                    <div v-html="inDropDown ? highlightString(option) : option"></div>
                     <span v-if="inDropDown && apps[option].__app_name">
-                            &nbsp;- {{apps[option].__app_name}}
+                            &nbsp;- {{highlightString(apps[option].__app_name)}}
                         </span>
                 </div>
-                <div v-if="inDropDown && apps[option].__app_owner" :class="!isSelected ? 'text-solstice-blue-opacity-60' : ''">
-                    {{apps[option].__app_owner}}
+                <div
+                    v-if="inDropDown && apps[option].__app_owner"
+                    :class="!isSelected ? 'text-solstice-blue-opacity-60' : ''"
+                    v-html="highlightString(apps[option].__app_owner)"
+                >
                 </div>
             </div>
         </template>
