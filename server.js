09009ec0915e6f3ad28df4d5942d78ba9cf0a03e
Maxime <maxiloc@gmail.com>
2019-10-08 22:49:11 +0200

    (misc) add inde-size to deployment

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.use('/perso-formula', serveStatic(__dirname + "/packages/perso-formula/dist"
 app.use('/relevance-testing', serveStatic(__dirname + "/packages/relevance-testing/dist"));
 app.use('/index-differ', serveStatic(__dirname + "/packages/index-differ/dist"));
 app.use('/dictionaries', serveStatic(__dirname + "/packages/dictionaries/dist"));
+app.use('/index-size', serveStatic(__dirname + "/packages/index-size/dist"));
 app.use((req, res) => {
     res.redirect("https://algolia-experimental.herokuapp.com/apps");
 });
