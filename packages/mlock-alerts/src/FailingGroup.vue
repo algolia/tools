893e7291038cde95da645b838c6b178238835405
Maxime <maxiloc@gmail.com>
2020-01-13 15:03:56 +0100

    (mlock-alerts) fix hardcoded values

--- packages/mlock-alerts/src/FailingGroup.vue
+++ packages/mlock-alerts/src/FailingGroup.vue
@@ -3,39 +3,32 @@
         <h2 class="text-solstice-blue-opacity-80">
             {{groupName}}
         </h2>
-        <div v-for="(cluster, _) in group" class="mt-16">
+        <div v-for="cluster in Object.keys(group).sort((a, b) => { return a.localeCompare(b); })" class="mt-16">
             <div class="text-lg">
                 <a
-                    :href="`https://metrics.wavefront.com/dashboard/cluster-analysis#(p:(cluster:(l:'Cluster%20regex',v:'${cluster.cluster}-*'),cluster-name:${cluster.cluster}))`"
+                    :href="`https://metrics.wavefront.com/dashboard/cluster-analysis#(p:(cluster:(l:'Cluster%20regex',v:'${group[cluster].cluster}-*'),cluster-name:${group[cluster].cluster}))`"
                     target="_blank"
                     class="text-nebula-blue cursor-pointer hover:underline"
-                >
-                    {{cluster.cluster}}
-                </a>
-                <span v-if="cluster.dsns.length > 0">
+                >{{group[cluster].cluster}}</a>
+                <span v-if="group[cluster].dsns.length > 0">
                     |
-                    <span v-for="dsn in cluster.dsns">
+                    <span v-for="dsn in group[cluster].dsns">
                         <a
                             :href="`https://metrics.wavefront.com/dashboard/cluster-analysis#(p:(cluster:(l:'Cluster%20regex',v:'${dsn}-*'),cluster-name:${dsn}))`"
                             target="_blank"
                             class="text-nebula-blue cursor-pointer hover:underline"
-                        >
-                            {{dsn}}
-                        </a>
-                    </span>
-                    |
+                        >{{dsn}}</a><span v-if="dsn !== group[cluster].dsns[group[cluster].dsns.length - 1]">,&nbsp;</span>
+                    </span>&nbsp;|
                     <a
                         :href="`https://metrics.wavefront.com/dashboards/cluster-analysis#_v01(p:(cluster:(l:'Cluster%20regex',v:'${cluster
-                        .cluster}-*${cluster.dsns.map(() => '%20or%20source=t25-usw-*').join('')}'),cluster-name:${cluster.cluster}))`"
+                        .cluster}-*${group[cluster].dsns.map((dsn) => `%20or%20source=${dsn}-*`).join('')}'),cluster-name:${group[cluster].cluster}))`"
                         target="_blank"
                         class="text-nebula-blue cursor-pointer hover:underline"
-                    >
-                            all
-                    </a>
+                    >all</a>
                 </span>
             </div>
             <div class="ml-12">
-                <div v-for="(user, email) in cluster.users" class="mb-8">
+                <div v-for="(user, email) in group[cluster].users" class="mb-8">
                     <div>{{user.company}} ({{user.email}})</div>
                     <div>
                         <span v-for="app in user.apps">
@@ -43,9 +36,8 @@
                                 :href="app.adminUrl"
                                 target="_blank"
                                 class="text-nebula-blue cursor-pointer hover:underline"
-                            >
-                                {{app.appId}}
-                            </a>
+                            >{{app.appId}}</a>
+                            <span v-if="app.appId !== user.apps[user.apps.length - 1].appId">,&nbsp;</span>
                         </span>
                     </div>
                 </div>
