c3f4b4522b28b44c14452334b335646d8af99625
Maxime <maxiloc@gmail.com>
2020-01-15 15:34:54 +0100

    (mlock-alerts) Add infrabooking link; refactor ui

--- packages/mlock-alerts/src/FailingGroup.vue
+++ packages/mlock-alerts/src/FailingGroup.vue
@@ -3,48 +3,69 @@
         <h2 class="text-solstice-blue-opacity-80">
             {{groupName}}
         </h2>
-        <div v-for="cluster in Object.keys(group).sort((a, b) => { return a.localeCompare(b); })" class="mt-16">
-            <div class="text-lg">
-                <a
-                    :href="`https://metrics.wavefront.com/dashboard/cluster-analysis#(p:(cluster:(l:'Cluster%20regex',v:'${group[cluster].cluster}-*'),cluster-name:${group[cluster].cluster}))`"
-                    target="_blank"
-                    class="text-nebula-blue cursor-pointer hover:underline"
-                >{{group[cluster].cluster}}</a>
-                <span v-if="group[cluster].dsns.length > 0">
-                    |
-                    <span v-for="dsn in group[cluster].dsns">
+        <div v-for="cluster in Object.keys(group).sort((a, b) => { return a.localeCompare(b); })" class="mt-24 pb-12 border-b border-proton-grey-opacity-40">
+            <div class="flex">
+                <div class="w-third">
+                    <div v-for="(user, email) in group[cluster].users" class="mb-8">
+                        <div>{{user.company}}</div>
+                        <div>{{user.email}}</div>
+                        <div>
+                            <div v-for="app in user.apps">
+                                <a
+                                    :href="app.adminUrl"
+                                    target="_blank"
+                                    class="text-nebula-blue cursor-pointer hover:underline"
+                                >{{app.appId}}</a>
+                                <span v-if="app.appId !== user.apps[user.apps.length - 1].appId">,&nbsp;</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div class="flex" v-if="group[cluster].dsns.length > 0">
+                        <span class="w-60">all</span>
+                        <a
+                            :href="`https://metrics.wavefront.com/dashboards/cluster-analysis#_v01(p:(cluster:(l:'Cluster%20regex',v:'${group[cluster]
+                    .cluster}-*${group[cluster].dsns.map((dsn) => `%20or%20source=${dsn}-*`).join('')}'),cluster-name:${group[cluster].cluster}))`"
+                            target="_blank"
+                            class="text-nebula-blue cursor-pointer hover:underline"
+                        >Wavefront</a>
+                    </div>
+                    <div class="flex">
+                        <span class="w-60">{{group[cluster].cluster}}</span>
+                        <a
+                            :href="`https://metrics.wavefront.com/dashboard/cluster-analysis#(p:(cluster:(l:'Cluster%20regex',v:'${group[cluster].cluster}-*'),cluster-name:${group[cluster].cluster}))`"
+                            target="_blank"
+                            class="text-nebula-blue cursor-pointer hover:underline"
+                        >
+                            Wavefront
+                        </a>
+                        <a
+                            :href="`https://infrabooking.algolia.com/infra/clusters?query=${group[cluster].cluster}`"
+                            target="_blank"
+                            class="ml-12 text-nebula-blue cursor-pointer hover:underline"
+                        >
+                            InfraBooking
+                        </a>
+                    </div>
+                    <div class="flex" v-if="group[cluster].dsns.length > 0" v-for="dsn in group[cluster].dsns">
+                        <span class="w-60">{{dsn}}</span>
                         <a
                             :href="`https://metrics.wavefront.com/dashboard/cluster-analysis#(p:(cluster:(l:'Cluster%20regex',v:'${dsn}-*'),cluster-name:${dsn}))`"
                             target="_blank"
                             class="text-nebula-blue cursor-pointer hover:underline"
-                        >{{dsn}}</a><span v-if="dsn !== group[cluster].dsns[group[cluster].dsns.length - 1]">,&nbsp;</span>
-                    </span>&nbsp;|
-                    <a
-                        :href="`https://metrics.wavefront.com/dashboards/cluster-analysis#_v01(p:(cluster:(l:'Cluster%20regex',v:'${group[cluster]
-                        .cluster}-*${group[cluster].dsns.map((dsn) => `%20or%20source=${dsn}-*`).join('')}'),cluster-name:${group[cluster].cluster}))`"
-                        target="_blank"
-                        class="text-nebula-blue cursor-pointer hover:underline"
-                    >all</a>
-                </span>
-            </div>
-            <div class="ml-12">
-                <div v-for="(user, email) in group[cluster].users" class="mb-8">
-                    <div>{{user.company}} ({{user.email}})</div>
-                    <div>
-                        <span v-for="app in user.apps">
-                            <a
-                                :href="app.adminUrl"
-                                target="_blank"
-                                class="text-nebula-blue cursor-pointer hover:underline"
-                            >{{app.appId}}</a>
-                            <span v-if="app.appId !== user.apps[user.apps.length - 1].appId">,&nbsp;</span>
-                        </span>
+                        >Wavefront</a>
+                        <a
+                            :href="`https://infrabooking.algolia.com/infra/clusters?query=${dsn}`"
+                            target="_blank"
+                            class="ml-12 text-nebula-blue cursor-pointer hover:underline"
+                        >
+                            InfraBooking
+                        </a>
                     </div>
                 </div>
             </div>
         </div>
-        <div></div>
-        <!--<pre>{{JSON.stringify(group, null, 2)}}</pre>-->
     </div>
 
 </template>
