c3d3d789c3e2a3ce38784b47bd66f41ce1aa850f
Maxime <maxiloc@gmail.com>
2020-04-01 20:27:53 +0200

    (misc) revert

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
             return res.redirect('https://' + req.headers.host + req.url);
         }
         if (req.hostname === 'algolia-experimental.herokuapp.com') {
-            //return res.redirect(`https://tools.algolia.com${req.url}`);
+            return res.redirect(`https://tools.algolia.com${req.url}`);
         }
     }
     return next();
