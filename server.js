cd896f987c43f33ff4b31de71e66b5e1c92f037b
Maxime <maxiloc@gmail.com>
2020-09-11 13:37:46 +0200

    (server) better analytics function

--- server.js
+++ server.js
@@ -7,25 +7,6 @@ const app = express();
 
 const ua = require('universal-analytics')
 
-app.use(function (req, res, next) {
-    const visitor = ua('UA-32446386-39');
-
-    const cookie = req.headers.cookie || '';
-    if (req.url.length > 0 && cookie.includes('laravel_session') && !req.url.includes('css') && !req.url.includes('js')) {
-        visitor.pageview({
-            dp: req.url,
-            dh: req.headers.host,
-            uip: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
-            ua: req.headers['user-agent'],
-            dr: req.headers.referrer || req.headers.referer,
-            de: req.headers['accept-encoding'],
-            ul: req.headers['accept-language']
-        }).send();
-    }
-
-    next();
-});
-
 app.use((req, res, next) => {
     if (process.env.NODE_ENV === 'production') {
         if (req.headers['x-forwarded-proto'] !== 'https') {
@@ -44,24 +25,30 @@ app.use(history({
     ]
 }));
 
+const apps = ['css', 'apps', 'logs', 'metaparams', 'index-manager', 'perso-formula', 'relevance-testing', 'index-differ', 'index-analyzer', 'dictionaries', 'index-size', 'infra-watch', 'transform', 'insights-ui', 'mercari', 'v8', 'attribute-proximity'];
 
-app.use('/css', serveStatic(__dirname + "/packages/css/dist"));
-app.use('/apps', serveStatic(__dirname + "/packages/apps/dist"));
-app.use('/logs', serveStatic(__dirname + "/packages/logs/dist"));
-app.use('/metaparams', serveStatic(__dirname + "/packages/metaparams/dist"));
-app.use('/index-manager', serveStatic(__dirname + "/packages/index-manager/dist"));
-app.use('/perso-formula', serveStatic(__dirname + "/packages/perso-formula/dist"));
-app.use('/relevance-testing', serveStatic(__dirname + "/packages/relevance-testing/dist"));
-app.use('/index-differ', serveStatic(__dirname + "/packages/index-differ/dist"));
-app.use('/index-analyzer', serveStatic(__dirname + "/packages/index-analyzer/dist"));
-app.use('/dictionaries', serveStatic(__dirname + "/packages/dictionaries/dist"));
-app.use('/index-size', serveStatic(__dirname + "/packages/index-size/dist"));
-app.use('/infra-watch', serveStatic(__dirname + "/packages/infra-watch/dist"));
-app.use('/transform', serveStatic(__dirname + "/packages/transform/dist"));
-app.use('/insights-ui', serveStatic(__dirname + "/packages/insights-ui/dist"));
-app.use('/mercari', serveStatic(__dirname + "/packages/mercari/dist"));
-app.use('/v8', serveStatic(__dirname + "/packages/v8/dist"));
-app.use('/attribute-proximity', serveStatic(__dirname + "/packages/attribute-proximity/dist"));
+apps.forEach((appName) => {
+    const serveStaticFunc = serveStatic(__dirname + `/packages/${appName}/dist`);
+
+    app.use(`/${appName}`, function (req, res, next) {
+        const visitor = ua('UA-32446386-39');
+
+        const cookie = req.headers.cookie || '';
+        if (req.url.length > 0 && cookie.includes('laravel_session') && !req.url.includes('css') && !req.url.includes('js')) {
+            visitor.pageview({
+                dp: req.url,
+                dh: req.headers.host,
+                uip: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+                ua: req.headers['user-agent'],
+                dr: req.headers.referrer || req.headers.referer,
+                de: req.headers['accept-encoding'],
+                ul: req.headers['accept-language']
+            }).send();
+        }
+
+        return serveStaticFunc(req, res, next);
+    });
+});
 
 // Legacy
 app.use('/mlock-alerts', (req, res) => res.redirect('/infra-watch'));
