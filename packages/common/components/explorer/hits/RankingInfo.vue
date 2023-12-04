4dc601898bbd14dfa642bc030d3e35157783a2e4
Steven Evans <steven.evans@algolia.com>
2023-12-04 13:46:46 -0500

    feat(metaparams): add neural search info to RankingInfo

--- packages/common/components/explorer/hits/RankingInfo.vue
+++ packages/common/components/explorer/hits/RankingInfo.vue
@@ -6,6 +6,7 @@
         <div>
             <div
                 v-for="criterion in criteria"
+                :key="criterion.label"
                 class="flex text-cosmos-black-opacity-70"
             >
                 <div class="w-128 text-right truncate">
@@ -32,18 +33,35 @@
                     </div>
                 </div>
             </div>
-            <div v-if="item._rankingInfo && item._rankingInfo.personalization" class="mt-32">
-                <div class="w-128 text-right mb-8 uppercase tracking-wide text-xs text-nova-grey">
-                    Perso Info
+        </div>
+        <div v-if="item._rankingInfo && item._rankingInfo.personalization" class="mt-16">
+            <div class="w-128 text-right mb-8 uppercase tracking-wide text-xs text-nova-grey">
+                Perso Info
+            </div>
+            <div v-for="k in Object.keys(item._rankingInfo.personalization)" :key="k" class="flex text-cosmos-black-opacity-70">
+                <div class="w-128 text-right truncate">
+                    {{k}}
                 </div>
-                <div v-for="k in Object.keys(item._rankingInfo.personalization)" class="flex text-cosmos-black-opacity-70">
-                    <div class="w-128 text-right truncate">
-                        {{k}}
+                <div class="ml-12 flex flex-wrap" style="width: calc(100% - 140px)">
+                    <div class="truncate hover:overflow-visible px-2 mr-1">
+                        {{item._rankingInfo.personalization[k]}}
                     </div>
-                    <div class="ml-12 flex flex-wrap" style="width: calc(100% - 140px)">
-                        <div class="truncate hover:overflow-visible px-2 mr-1">
-                            {{item._rankingInfo.personalization[k]}}
-                        </div>
+                </div>
+            </div>
+        </div>
+        <div v-if="item._rankingInfo && hasNeuralScores" class="mt-16">
+            <div class="w-128 text-right mb-8 uppercase tracking-wide text-xs text-nova-grey">
+                Neural Merge Info
+            </div>
+            <div v-for="mergeInfo in neuralMergeInfo"
+                 :key="mergeInfo.label"
+                 class="flex text-cosmos-black-opacity-70">
+                <div class="w-128 text-right truncate">
+                    {{ mergeInfo.label }}
+                </div>
+                <div class="ml-12 flex flex-wrap" style="width: calc(100% - 140px)">
+                    <div class="truncate hover:overflow-visible px-2 mr-1">
+                        {{ mergeInfo.val }}
                     </div>
                 </div>
             </div>
@@ -99,6 +117,67 @@
                 });
 
                 return criterias.filter((criterion) => { return criterion.val !== null || criterion.oldVal !== null });
+            },
+            hasNeuralScores: function () {
+                return ['keywordScore', 'neuralScore', 'semanticScore'].some(
+                    (scoreType) => Object.hasOwn(this.item._rankingInfo, scoreType)
+                )
+            },
+            neuralMergeInfo: function () {
+                const position = (rankingInfo, key) => {
+                    let position;
+                    if (rankingInfo.mergeInfo !== undefined && rankingInfo.mergeInfo[key] !== undefined) {
+                        position = parseInt(rankingInfo.mergeInfo[key]) + 1;
+                    }
+                    return position;
+                }
+
+                const score = (rankingInfo, key) => {
+                    let score;
+                    if (rankingInfo[key] !== undefined) {
+                        score = Math.round(parseFloat(rankingInfo[key])*10000)/10000;
+                        if (Number.isInteger(score)) {
+                            score = score.toFixed(1);
+                        }
+                    }
+                    return score
+                }
+
+                const value = (position, score) => {
+                    let value = position === undefined ? "" : "position: " + position;
+                    value += score === undefined ? "" : (value === "" ? "score: " + score : " (score: " + score + ")")
+                    return value === "" ? undefined : value;
+                }
+
+                const mergeInfo = [
+                    {
+                        label: 'keyword',
+                        val: value(
+                            position(this.item._rankingInfo, 'keyword'),
+                            score(this.item._rankingInfo, 'keywordScore')
+                        )
+                    },
+                    {
+                        label: 'vector',
+                        val: value(
+                            position(this.item._rankingInfo, 'semantic'),
+                            score(this.item._rankingInfo, 'semanticScore')
+                        )
+                    },
+                    {
+                        label: 'filters',
+                        val: value(position(this.item._rankingInfo, 'filters'))
+                    },
+                    {
+                        label: 'geo',
+                        val: value(position(this.item._rankingInfo, 'geo'))
+                    },
+                    {
+                        label: 'final',
+                        val: value(this.i + 1, score(this.item._rankingInfo, 'neuralScore'))
+                    }
+                ];
+                return mergeInfo.filter((ranking) => ranking.val !== undefined);
             }
         },
         methods: {
