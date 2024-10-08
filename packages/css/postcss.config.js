47807746a55a19cffcd7abbf4ad8a993bec4c261
Christopher Hawke <69921547+cdhawke@users.noreply.github.com>
2024-10-08 08:33:34 -0400

    chore: postcss@8.4.31 and remove postcss processor

--- packages/css/postcss.config.js
+++ packages/css/postcss.config.js
@@ -1,6 +1,5 @@
 module.exports = {
     plugins: [
-        require("@csstools/postcss-sass"),
         require("tailwindcss"),
         require("autoprefixer"),
         ...(process.env.NODE_ENV === "production"
