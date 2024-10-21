587d73025cb1d478f0b59e059ae40925cd3e4839
Christopher Hawke <69921547+cdhawke@users.noreply.github.com>
2024-10-21 10:21:17 -0400

    feat(apps): add tool name to user agent  (#53)
    * feat: add logger.js and format helpers
    
    * chore: format server
    
    * fix: tooltip
    
    * feat: add client logger to post to /logs
    
    * feat: logging addition
    
    * fea: simplify logger
    
    * feat: attempt validation for host for codacy
    
    * feat: try sanitizing instead
    
    * feat: remove url on https upgrade
    
    * feat: from a valid domain list
    
    * fix: ridiculous codacy warnings
    
    * feat: add globaldata appname storage and logging
    
    * fix: console logs and props

--- packages/common/components/header/AppHeader.vue
+++ packages/common/components/header/AppHeader.vue
@@ -1,19 +1,32 @@
 <template>
-    <div>
-        <div class="flex flex-col justify-center w-full bg-white z-10 border-b border-solid border-proton-grey-opacity-80" style="min-height: 94px; ">
-            <div class="flex px-16">
-                <div class="flex w-full">
-                    <div class="flex items-center text-telluric-blue mr-16 text-lg">
-                        <algolia-logo-experimental class="w-20 h-20 mr-8"/>
-                        <div class="mr-4 mt-2"><a :href="`${hostUrl}/apps`">Tools</a></div>
-                        <chevron-left v-if="appName" class="w-12 h-12 rotate-180 mr-4" />
-                        <div v-if="appName" class="mt-2 font-bold">{{appName}}</div>
-                    </div>
-                    <slot />
-                </div>
+  <div>
+    <div
+      class="flex flex-col justify-center w-full bg-white z-10 border-b border-solid border-proton-grey-opacity-80"
+      style="min-height: 94px; "
+    >
+      <div class="flex px-16">
+        <div class="flex w-full">
+          <div class="flex items-center text-telluric-blue mr-16 text-lg">
+            <algolia-logo-experimental class="w-20 h-20 mr-8" />
+            <div class="mr-4 mt-2">
+              <a :href="`${hostUrl}/apps`">Tools</a>
             </div>
+            <chevron-left
+              v-if="appName"
+              class="w-12 h-12 rotate-180 mr-4"
+            />
+            <div
+              v-if="appName"
+              class="mt-2 font-bold"
+            >
+              {{ appName }}
+            </div>
+          </div>
+          <slot />
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -22,8 +35,14 @@
 
     export default {
         name: 'AppHeader',
-        props: ['appName'],
         components: {AlgoliaLogoExperimental, ChevronLeft},
+        props: {
+            appName: {
+                type: String,
+                required: false,
+                default: ''
+            }
+        },
         computed: {
             hostUrl: function () {
                 if (process.env.NODE_ENV === "production" && process.env.VUE_APP_TOOLS_INTERNAL_ENDPOINT && window.currentUserEmail && window.currentUserEmail.endsWith('@algolia.com')) {
@@ -31,6 +50,17 @@
                 }
                 return '';
             }
-        }
+        },
+        mounted() {
+            this.storeData();
+        },
+        methods: {
+            storeData() {
+                window.globalData = {
+                    ...window.globalData,
+                    appName: this.appName
+                };
+            },
+        },
     }
 </script>
